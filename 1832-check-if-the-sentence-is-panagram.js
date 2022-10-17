0ddf920ef2069706061a9e76b1dead64198d8565
HJeong1200 <hjeong1200@gmail.com>
2022-10-17 16:36:58 +0900

    221017

---
+++ 1832-check-if-the-sentence-is-panagram.js
@@ -0,0 +1,33 @@
+var checkIfPangram = function (sentence) {
+  let map = new Map();
+  let size = 0;
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (!map.has(sentence[i])) {
+      map.set(sentence[i], 1);
+      size++;
+    }
+  }
+
+  if (size === 26) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * Use Set instead of Map!!
+ */
+
+var checkIfPangram = function (sentence) {
+  let set = new Set();
+  for (let i = 0; i < sentence.length; i++) {
+    set.add(sentence[i]);
+    if (set.size === 26) {
+      return true;
+    }
+  }
+
+  return false;
+};
---
+++ 70-climbing-stairs.js
@@ -0,0 +1,65 @@
+const c = (n, i) => {
+  let child = 1;
+  let parent = 1;
+
+  for (let j = n - i; j > n - i - i; j--) {
+    child *= j;
+  }
+
+  for (let j = 1; j <= i; j++) {
+    parent *= j;
+  }
+
+  return child / parent;
+};
+
+var climbStairs = function (n) {
+  let counter = 0;
+
+  for (let i = 0; 2 * i <= n; i++) {
+    counter += c(n, i);
+  }
+
+  return counter;
+};
+
+// n은 계단의 수
+// iterate i
+// 1계단 올라가는 one = n - 2 * i
+// 2계단 올라가는 two = 2 * i
+
+// 경우의 수 n - i C i
+
+// 4C0, 3C1, 2C2
+
+/**
+ * 피보나치..??
+ *
+ * n번째 계단으로 가는 방법:
+ *      n-1번째 계단에서 1칸 올라간다
+ *      n-2번째 계단에서 2칸 올라간다
+ *
+ * n번째 계단으로 올라가는 모든 경우의 수
+ *  =   n-1번째 계단으로 가는 모든 경우의 수
+ *  +   n-2번째 계단으로 가는 모든 경우의 수
+ *
+ * ...
+ *
+ * 3번째 계단으로 올라가는 모든 경우의 수
+ *  =   2번째 계단으로 가는 모든 경우의 수
+ *  +   1번째 계단으로 가는 모든 경우의 수
+ *
+ * => 피보나치 수열이네!
+ *
+ */
+
+var climbStairs = function (n) {
+  let base = 1;
+  let result = 1;
+
+  for (let i = 1; i < n; i++) {
+    [result, base] = [base + result, result];
+  }
+
+  return result;
+};
