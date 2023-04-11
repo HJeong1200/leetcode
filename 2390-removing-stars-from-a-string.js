26ce056b2a043af959f8aff7cdd0da84c08835f5
HJeong1200 <hjeong1200@gmail.com>
2023-04-12 00:52:25 +0900

    230411

---
+++ 2390-removing-stars-from-a-string.js
@@ -0,0 +1,13 @@
+var removeStars = function (s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "*") {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
+    }
+  }
+
+  return stack.join("");
+};
